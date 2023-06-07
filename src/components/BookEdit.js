293656bccc7ed7333737efbe922ce27004f64a7e
loganathanravi <loguaustin7788@gmail.com>
2023-06-07 13:03:49 +0530

    removing prop

--- src/components/BookEdit.js
+++ src/components/BookEdit.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { useState } from 'react';
 
-function BookEdit({book,onEdit,onSubmit}) {
+function BookEdit({book,onSubmit}) {
    const [title,setTitle]=useState(book.title);
    const handleChange=(event)=>{
     setTitle(event.target.value);
    };
   const handleSubmit=(event)=>{
     event.preventDefault();
-  onEdit(book.id,title);
-  onSubmit();
+  
+  onSubmit(book.id,title);
   };
   return <form onSubmit={handleSubmit} className='book-edit'>
     <label>Title</label>
